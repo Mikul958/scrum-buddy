622f6f390fee92f6f059c7569b92beb29722e004
apacicco <apacicco@email.sc.edu>
2023-10-10 15:25:15 -0400

    UML added

---
+++ project/Column.java
@@ -0,0 +1,14 @@
+package project;
+
+
+import java.util.*;
+public class Columns {
+    private String title;
+
+    private ArrayList<Task> tasks;//no task class yet
+
+    public Columns()
+    {
+
+    }
+}
