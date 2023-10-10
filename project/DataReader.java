9404e5921e9c0386f34c180b1bdd399400c6705c
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-10-10 16:00:13 -0400

    Added: Account.java, AccountManager.java, ScrumSystem.java, DataReader.java

---
+++ project/DataReader.java
@@ -0,0 +1,17 @@
+package project;
+/**
+ * @author Miles Wedeking
+ */
+public class DataReader {
+    
+}
+
+public ArrayList<Project> getAccounts(){
+
+    return;
+}
+
+public ArrayList<Account> getAccounts(){
+ 
+    return;
+}
