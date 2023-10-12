525dde8c6252d3b3c19a6580f911d26fb80eaaa9
Mikul958 <michaelpikula958@gmail.com>
2023-10-12 13:58:33 -0400

    Fixed #36

--- project/DataReader.java
+++ project/DataReader.java
@@ -1,6 +1,10 @@
 package project;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  * @author Miles Wedeking
--- project/DataWriter.java
+++ project/DataWriter.java
@@ -1,5 +1,11 @@
 package project;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 /**
  * @author Cade Phillips
  */
