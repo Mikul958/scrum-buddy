77466e734adbe301286682241fe5767cc4cb7c00
Mikul958 <michaelpikula958@gmail.com>
2023-10-12 14:22:28 -0400

    Working on #16

--- project/Column.java
+++ project/Column.java
@@ -12,19 +12,31 @@ public class Column
 
     public Column(String title)
     {
-
+        this.title = title;
+        tasks = new ArrayList<Task>();
     }
 
-    public void addTask(String name, Category category)
+    public void addNewFeature(String name, int priority, String reasoning)
     {
-
+        // TODO figure out parameters
+    }
+    public void addBug(String name, int priority, Account testerAccount)
+    {
+        // TODO figure out parameters
     }
     public void removeTask(Task task)
     {
-
+        for (int i=0; i<tasks.size(); i++)
+        {
+            if (tasks.get(i).equals(task))
+                tasks.remove(i);
+        }
     }
     public String toString()
     {
-        return null;
+        String out = "Column: " + title;
+        for (int i=0; i< tasks.size(); i++)
+            title += "\n" + tasks.get(i);
+        return out;
     }
 }
