5bdff00102a5e468205a7bf2037447dd7a95cb3f
Mikul958 <michaelpikula958@gmail.com>
2023-10-12 14:00:59 -0400

    Readded category.java to fix capitalization

---
+++ project/Category.java
@@ -0,0 +1,20 @@
+package project;
+
+/**
+ * @author Michael Pikula
+ */
+public enum Category
+{
+    BUSINESS("Business"),
+    DEVELOPMENT("Development"),
+    EDUCATION("Education"),
+    PERSONAL("Personal"),
+    OTHER("Other");
+
+    public final String description;
+
+    private Category(String description)
+    {
+        this.description = description;
+    }
+}
