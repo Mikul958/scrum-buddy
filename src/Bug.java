cf432c8b68c1560cb935d30a835678ff103ac292
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-10-14 13:04:54 -0400

    Bug class corrections

--- src/Bug.java
+++ src/Bug.java
@@ -1,11 +1,10 @@
 package src;
-
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class Bug
 {
-    public int BUG_COUNT = 0;
+    public int BUG_COUNT = 0;   //Global bug count;
     private String name;
     private int priority;
     private Account account;
@@ -14,7 +13,7 @@ public class Bug
     public Bug(String name, int priority, Account account)
     {
         //Name
-        if(name != null){
+        if(name != null){   //TODO name isValid() method.
             this.name = name;
             BUG_COUNT++;
         }
@@ -22,6 +21,7 @@ public class Bug
             this.name = "Bug"+BUG_COUNT;
             BUG_COUNT++;
         }
+
         //Priority
         if(priority < 0){
             this.priority = 0;
@@ -29,8 +29,9 @@ public class Bug
         else{
             this.priority = priority;
         }
+
         //Account
-        if(account != null){
+        if(account != null){    //TODO account isValid() method.
             this.account = account;
         }
         else{
@@ -38,9 +39,9 @@ public class Bug
         }
 
     }
-    public Bug(UUID id, String name, int priority, Account account)
+    public Bug(UUID id, String name, int priority, Account tester)
     {
-        //TODO Need Mike's help here. <-
+        //TODO Need Mike's help here <-
     }
 
     public String toString()
