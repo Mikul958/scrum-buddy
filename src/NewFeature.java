825d9c106860f86de6e081971f52cb4083654148
Cade Phillips <cadephillips4@Cades-MacBook-Air.local>
2023-10-15 13:43:41 -0400

    fixed #15

--- src/NewFeature.java
+++ src/NewFeature.java
@@ -15,11 +15,14 @@ public class NewFeature extends Task
     // Don't overcomplicate, use Bug class as a guideline.
     public NewFeature(String name, int priority, String reasoning)
     {
-
+        super(name, priority);
+        this.reasoning = reasoning;
+        this.todo = new ArrayList<String>();
     }
     public NewFeature(UUID id, String name, int priority, String reasoning)
     {
-
+        super(id, name, priority);
+        this.reasoning = reasoning;
     }
 
     public String toString()
