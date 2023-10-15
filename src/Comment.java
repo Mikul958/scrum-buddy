a964a1a7ec1fbcc3810c0f3b4c641c29eb509f11
Mikul958 <michaelpikula958@gmail.com>
2023-10-14 20:22:11 -0400

    Updated Comment class, added addReply() and deleteReply()

--- src/Comment.java
+++ src/Comment.java
@@ -5,41 +5,45 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+/**
+ * @author Miles Wedeking, Michael Pikula
+ */
 public class Comment
 {
-    private Account user;
     private LocalDateTime dateTime;
+    private Account user;
     private String content;
     private ArrayList<Comment> replies;
-    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    private DateTimeFormatter dtf;  // I don't know how to use this, is it one-time use?
 
     public Comment(Account user, String content)
     {
-        //Date
-        LocalDateTime now = LocalDateTime.now();
-        this.dateTime = now;
-
-        //User
-        if(user.getIsVerified() == true){
-            this.user = user;
-        }
-        else{
-            this.user = null;
-        }
-
-        //Content
-        if(content == null){
-            this.content = "None.";
-        }
-        else{
-            this.content = content;
-        }
+        this.dateTime = LocalDateTime.now();  // Set date to time of construction.
+        this.user = user;
+        this.content = content;
+        this.replies = new ArrayList<Comment>();
+        this.dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
     }
 
     public String toString()
     {
-        return "User: "+user+
-        "\nDate: "+dateTime+
-        "\nContent"+content;
+        String out = "User: ";
+
+        if (user != null)
+            out += user;
+        else
+            out += "anonymous";
+
+        out += "\nDate: " + dateTime + "\nContent: " + content;
+        return out;
+    }
+    public void addReply(Account user, String content)
+    {
+        Comment newReply = new Comment(user, content);
+        replies.add(newReply);
+    }
+    public void deleteReply(Comment reply)
+    {
+        replies.remove(reply);
     }
 }
