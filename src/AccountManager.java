028b3bb5b01a70caf27a8010b6dd4bec07e1407f
Mikul958 <michaelpikula958@gmail.com>
2023-10-18 09:40:18 -0400

    Started work from scrum meeting.

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -16,13 +16,20 @@ public class AccountManager
         return null;
     }
 
-    public Account getAccount(String userName)
+    public Account getAccount(String username, String password)
     {    
+        for (int i=0; i<accounts.size(); i++)
+        {
+            if (username.equals(accounts.get(i).getUsername()) && password.equals(accounts.get(i).getPassword()))
+                return accounts.get(i);
+        }
         return null;
     }
-    public void createAccount(String firstName, String lastName, String email, String userName, String passWord)
+    public void createAccount(String userName, String passWord, String email, String firstName, String lastName, )
     {
-
+        // TODO make sure username is unique
+        Account newAccount = new Account(username, password, email, firstName, lastName, false);
+        accounts.add(newAccount);
     }
     public void deleteAccount(Account account)
     {
