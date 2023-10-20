f645199e206b4f44e4687673d7eac5a04072a3f5
Mikul958 <michaelpikula958@gmail.com>
2023-10-19 21:48:21 -0400

    Added toString() method

--- src/Category.java
+++ src/Category.java
@@ -1,6 +1,7 @@
 package src;
 
 /**
+ * An enumeration containing all categories that a project can select.
  * @author Michael Pikula
  */
 public enum Category
@@ -13,8 +14,16 @@ public enum Category
 
     public final String description;
 
+    /**
+     * @param description
+     */
     private Category(String description)
     {
         this.description = description;
+    }
+
+    public String toString()
+    {
+        return description;
     }
 }
