0d3348e86d5fe070b4b5a75a1e3337a97fbbc8a1
Mikul958 <michaelpikula958@gmail.com>
2023-10-23 18:22:09 -0400

    Removed reply functionality

--- src/Comment.java
+++ src/Comment.java
@@ -13,7 +13,6 @@ public class Comment
     private LocalDateTime dateTime;
     private Account user;
     private String content;
-    private ArrayList<Comment> replies;
     private DateTimeFormatter dateFormat;
 
     /**
@@ -26,7 +25,6 @@ public class Comment
         this.dateTime = LocalDateTime.now();  // Set date to time of construction.
         this.user = user;
         this.content = content;
-        this.replies = new ArrayList<Comment>();
         this.dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd 'at' HH:mm:ss");
     }
     /**
@@ -40,7 +38,6 @@ public class Comment
         this.dateTime = dateTime;
         this.user = user;
         this.content = content;
-        this.replies = new ArrayList<Comment>();
         this.dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd 'at' HH:mm:ss");
     }
 
@@ -59,24 +56,5 @@ public class Comment
 
         out += "\nDate: " + dateFormat.format(dateTime) + "\nContent: " + content;
         return out;
-    }
-    /**
-     * Creates a reply with the specified user and content and adds it to the list this comment's replies.
-     * @param user The user making the reply to this comment.
-     * @param content The reply being made to this comment.
-     */
-    public void addReply(Account user, String content)
-    {
-        Comment newReply = new Comment(user, content);
-        replies.add(newReply);
-    }
-    /**
-     * Deletes the specified reply to this comment if it exists.
-     * @param reply The reply to be removed.
-     * @return true if the specified reply exists in the list.
-     */
-    public boolean deleteReply(Comment reply)
-    {
-        return replies.remove(reply);
     }
 }
