25f743e14488c06bae1c8227b414d8d1b04bdcfb
mwwedeking <111778304+mwwedeking@users.noreply.github.com>
2023-10-23 14:50:47 -0400

    Worked on #7, again

--- src/Project.java
+++ src/Project.java
@@ -40,8 +40,7 @@ public class Project
 
     public void setCategory(Category category)
     {
-        //TODO Is this good enough?
-        this.category = category;
+        this.category = category;       //Good?
     }
     public double viewProgress()
     {
@@ -54,6 +53,7 @@ public class Project
         return false;
     }
     /**
+     * Add an account as a contributor to a project.
      * @param account to be added to the list of contributors assigned on this project. 
      * If the account is not already on the list of contributors assigned to this project
      * then the account is added to the list of contributors.
@@ -70,7 +70,9 @@ public class Project
             System.out.println("Account could not be added.");
     }
     /**
+     * Remove an account as a contributor from a project.
      * @param account to be removed from the list of contributors assigned on this project. 
+     *
      * Loops through the current list on contributors on this project.
      * If the specified account exists on the list of contributors, then it is removed.
      * Otherwise, if it is not on the list. Then an error message is printed for the user.
@@ -81,24 +83,50 @@ public class Project
             contributors.remove(account);
         }
         else    
-            System.out.println("Account could not be removed.");
+            System.out.println("Account could not be removed.");    //Temp
     }
+    /**
+     * Adds a column to the project
+     * @param name of the project.
+     */
     public void addColumn(String name)
     {
         Column temp = new Column(name);
         columns.add(temp);
     }
+
     public void moveColumnAfter(Column column)
     {
         //TODO
     }
+    /**
+     * Removes a column from the project
+     * @param column column to be removed
+     */
     public void removeColumn(Column column)
     {
-        
+        if(containsColumn(column.getTitle())){
+            columns.remove(column);
+        }
+        else
+            System.out.println("Column could not be removed.");     //Temp 
     }
     public void addTask(Column column, String name, int priority)
     {
-
+        for(int i = 0; i < columns.size(); i++){
+            Column temp = columns.get(i);
+            if(temp == column){
+                //TODO Left off here
+                //Not sure if we want to use a parameter to define type???
+                //Otherwise,
+                //How can I access column[i].tasks[j] etc. (pseudo)
+                //Aiming for something like what is below:
+                //
+                //If(temp == column){
+                //  columns.get(i).addTask(name, priority);
+                //}
+            }                                                           
+        }
     }
     public void removeTask(Column column, Task task)
     {
