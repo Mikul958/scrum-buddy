a216a3c73a7893628921e1ce244ef096ebd28f40
Mikul958 <michaelpikula958@gmail.com>
2023-10-26 21:36:49 -0400

    Removed unused imports

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -3,8 +3,6 @@ package src;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.UUID;
-import java.time.LocalDateTime;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
