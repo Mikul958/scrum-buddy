ad9e15125b63448c9c4d89bea9ae417638f25bab
Mikul958 <michaelpikula958@gmail.com>
2023-11-02 14:19:42 -0400

    Added junit import statements

--- src/ProjectTest.java
+++ src/ProjectTest.java
@@ -1,1 +1,17 @@
-//hi
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class ProjectTest
+{
+    @Test
+    public void testing()
+    {
+        assertTrue(true);
+    }
+}
