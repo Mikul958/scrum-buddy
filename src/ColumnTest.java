ab79b9ef4745860df03f89f90c163d4b5e56aa8c
Cade Phillips <cadephillips4@Cades-MacBook-Air.local>
2023-11-07 14:25:18 -0500

    Working on Column test

---
+++ src/ColumnTest.java
@@ -0,0 +1,22 @@
+package src;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class ColumnTest 
+{
+ 
+    //TESTING FOR COLUMN CLASS
+    @Test
+
+    void testGetTaskByName() {
+        
+    }
+    
+
+}
