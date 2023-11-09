3953dbc5c382638459ebc0d94e30ead6e2f86ff6
Cade Phillips <cadephillips4@Cades-MacBook-Air.local>
2023-11-08 19:36:20 -0500

    working on columnTest

--- src/ColumnTest.java
+++ src/ColumnTest.java
@@ -13,11 +13,58 @@ public class ColumnTest
  
     //TESTING FOR COLUMN CLASS
     @Test
-
     void testGetTaskByName() {
+        Account acc = new Account("CadeAP", "pasa123", "cadephillips4@gmail.com", "Cade", "Phillips");
+        Project proj = new Project("exampleProj", Category.OTHER, acc);
+        Column col = new Column("exampleColumn");
+        Task task = new Task("exampleTask", 1);
+        col.addTask("exampleTask2", 2);
 
-        
+        assertEquals(task, col.getTaskByName("exampleTask"));
     }
+    //RESULT: FAILED?
+
+
+    @Test
+    void testAddTask() {
+        Account acc = new Account("testCade", "pass123!", "tester@gmail.com", "Cade", "Phillips");
+        Project proj = new Project("null", Category.PERSONAL, acc);
+        Column col = new Column("testColumn");
+        Task task = new Task("testTask", 1);
+        col.addTask("testTask", 1);
+        assertTrue(col.getTasks().contains(task));
+    }
+    //RESULT: FAILED?
+
+
+    @Test
+    void testRemoveTask() {
+        Account acc = new Account("testCade", "pass123!", "tester@gmail.com", "Cade", "Phillips");
+        Project proj = new Project("null", Category.PERSONAL, acc);
+        Column col = new Column("testColumn");
+        Task task = new Task("testTask", 1);
+        col.addTask("testTask", 1);
+        col.removeTask(task);
+        assertFalse(col.getTasks().contains(task));
+    }
+    //RESULT: PASSED
+
+
+    @Test
+    void testOrderTasks() {
+        Account acc = new Account("testCade", "pass123!", "tester@gmail.com", "Cade", "Phillips");
+        Project proj = new Project("null", Category.PERSONAL, acc);
+        Column col = new Column("testColumn");
+        Task task = new Task("testTask", 1);
+        //Task task2 = new Task("testTask2", 1);
+        //Task task3 = new Task("testTask3", 1);
+        col.addTask("testTask", 1);
+        col.addTask("testTask2", 1);
+        col.addTask("testTask3", 1);
+        assertEquals(task, col.getTasks().get(0));
+
+    }
+
     
 
 }
